a0a0b3e3c3294d89a052edc639fc7f273bc190dc
Luko Delidzhov <luko193@gmail.com>
2025-02-12 20:58:49 +0200

    Implemented update of cart status quantity and price when adding product to cart from product details page

--- ecommerce/src/app/components/product-details/product-details.component.ts
+++ ecommerce/src/app/components/product-details/product-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../common/product';
+import { CartService } from '../../services/cart.service';
+import { CartItem } from '../../common/cart-item';
 
 @Component({
   selector: 'app-product-details',
@@ -15,6 +17,7 @@ export class ProductDetailsComponent implements OnInit{
   product!: Product;
 
   constructor(private productService: ProductService,
+              private cartService: CartService,
               private route: ActivatedRoute) { }
 
   ngOnInit(): void {
@@ -31,5 +34,10 @@ export class ProductDetailsComponent implements OnInit{
         this.product = data
       }
     );
+  }
+
+  addToCart() {
+    const cartItem = new CartItem(this.product);
+    this.cartService.addToCart(cartItem);
   }
 }
