d31f9c91e170fe6fa5713c7c38db19412c078aed
Luko Delidzhov <luko193@gmail.com>
2025-02-14 21:17:19 +0200

    + Added shopping cart details functionality

--- ecommerce/src/app/app.module.ts
+++ ecommerce/src/app/app.module.ts
@@ -13,10 +13,12 @@ import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 
 const routes: Routes = [
   {path: 'products/:id', component: ProductDetailsComponent},
   {path: 'products/category/:id', component: ProductListComponent},
+  {path: 'cart-details', component: CartDetailsComponent},
   {path: 'category', component: ProductListComponent},
   {path: 'products', component: ProductListComponent},
   {path: '', redirectTo: 'products', pathMatch: 'full'},
@@ -30,7 +32,8 @@ const routes: Routes = [
     ProductCategoryMenuComponent,
     SearchComponent,
     ProductDetailsComponent,
-    CartStatusComponent
+    CartStatusComponent,
+    CartDetailsComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
