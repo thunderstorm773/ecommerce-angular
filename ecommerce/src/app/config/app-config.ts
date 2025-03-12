d83a38a062fc8fb57e151f8348aa2c5a81705d5a
Luko Delidzhov <luko193@gmail.com>
2025-03-12 16:54:21 +0200

    + Using Okta authorization server with https

--- ecommerce/src/app/config/app-config.ts
+++ ecommerce/src/app/config/app-config.ts
@@ -4,7 +4,7 @@ export default {
         clientId: '0oannuz0507YIawDl5d7',
         baseUrl: 'https://dev-65742957.okta.com',
         issuer: 'https://dev-65742957.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 }
