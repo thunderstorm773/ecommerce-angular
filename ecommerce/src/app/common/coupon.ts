bc3af76379ce45b3459daac1afa636f1c7b45b11
Luko Delidzhov <luko193@gmail.com>
2025-04-26 23:14:56 +0300

    + Showing discount percent in members page

--- ecommerce/src/app/common/coupon.ts
+++ ecommerce/src/app/common/coupon.ts
@@ -1,6 +1,7 @@
 export class Coupon {
 
     constructor(public discountCode: string,
+                public discountPercent: number,
                 public validFrom: Date,
                 public validTo: Date,
                 public status: boolean) { }
