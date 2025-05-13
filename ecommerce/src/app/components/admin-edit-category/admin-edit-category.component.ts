7ff9badbd4af7605d76fe37f33e12631b76027f2
Luko Delidzhov <luko193@gmail.com>
2025-05-13 21:58:43 +0300

    + Add category name field into edit category page

--- ecommerce/src/app/components/admin-edit-category/admin-edit-category.component.ts
+++ ecommerce/src/app/components/admin-edit-category/admin-edit-category.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormValidator } from '../../validators/form-validator';
 
 @Component({
   selector: 'app-admin-edit-category',
@@ -7,6 +9,27 @@ import { Component } from '@angular/core';
   templateUrl: './admin-edit-category.component.html',
   styleUrl: './admin-edit-category.component.css'
 })
-export class AdminEditCategoryComponent {
+export class AdminEditCategoryComponent implements OnInit {
 
+  categoryFormGroup!: FormGroup;
+  
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+      this.editCategoryFormGroup();
+    }
+  
+    editCategoryFormGroup() {
+      this.categoryFormGroup = this.formBuilder.group({
+        categoryName: new FormControl('', [Validators.required, Validators.minLength(2), FormValidator.checkNotOnlyWhitespace])
+      });
+    }
+  
+    editCategory() {
+      console.log('editCategory()');
+    }
+  
+    get categoryName() {
+      return this.categoryFormGroup.get('categoryName');
+    }
 }
