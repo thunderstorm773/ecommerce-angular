dccba7947cdc90ba32c8ad9b2f5bb5bda95668bc
Luko Delidzhov <luko193@gmail.com>
2025-05-31 23:47:24 +0300

    + Base form for create new coupon + Refactoring

--- ecommerce/src/app/components/admin-add-coupon/admin-add-coupon.component.ts
+++ ecommerce/src/app/components/admin-add-coupon/admin-add-coupon.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastService } from '../../services/toast.service';
+import { FormValidator } from '../../validators/form-validator';
 
 @Component({
   selector: 'app-admin-add-coupon',
@@ -7,6 +11,56 @@ import { Component } from '@angular/core';
   templateUrl: './admin-add-coupon.component.html',
   styleUrl: './admin-add-coupon.component.css'
 })
-export class AdminAddCouponComponent {
+export class AdminAddCouponComponent implements OnInit {
 
+  couponFormGroup!: FormGroup;
+  isDisabled: boolean = false;
+  
+  constructor(private formBuilder: FormBuilder,
+              private router: Router,
+              private toastService: ToastService) {}
+
+
+  ngOnInit(): void {
+    this.createCouponFormGroup();
+  }
+
+  createCouponFormGroup() {
+    this.couponFormGroup = this.formBuilder.group({
+        discountCode: ['', {validators: [Validators.required, Validators.minLength(4), FormValidator.checkNotOnlyWhitespace]}],
+        discountPercent: [null, {validators: [Validators.required, Validators.min(1), Validators.max(30)]}],
+        validFrom: [null, {validators: [Validators.required]}],
+        validTo: [null, {validators: [Validators.required]}],
+        isActive: [false]
+    });
+  }
+
+  createNewCoupon() {
+    if (this.couponFormGroup.invalid) {
+      this.couponFormGroup.markAllAsTouched();
+      return;
+    }
+
+
+  }
+
+  get discountCode() {
+    return this.couponFormGroup.get('discountCode');
+  }
+
+  get discountPercent() {
+    return this.couponFormGroup.get('discountPercent');
+  }
+
+  get validFrom() {
+    return this.couponFormGroup.get('validFrom');
+  }
+
+  get validTo() {
+    return this.couponFormGroup.get('validTo');
+  }
+
+  get isActive() {
+    return this.couponFormGroup.get('isActive');
+  }
 }
