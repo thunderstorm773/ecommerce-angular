4ed3129bca3a14f27c785a4fca7464aa09ef9530
Luko Delidzhov <luko193@gmail.com>
2025-07-21 20:45:19 +0300

    + Removed unnecessary field from product class

--- ecommerce/src/app/common/product.ts
+++ ecommerce/src/app/common/product.ts
@@ -1,7 +1,6 @@
 export class Product {
 
     constructor(public id: number,
-                public sku: string,
                 public name: string,
                 public description: string,
                 public unitPrice: number,
