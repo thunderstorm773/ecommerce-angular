bcfc158e4e144a0d3f9ab537f097c6a5f5face41
Luko Delidzhov <luko193@gmail.com>
2025-07-24 23:46:40 +0300

    + Created base edit product component

--- ecommerce/src/app/components/admin-add-product/admin-add-product.component.ts
+++ ecommerce/src/app/components/admin-add-product/admin-add-product.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ProductService } from '../../services/product.service';
 import { ToastService } from '../../services/toast.service';
+import { FormValidator } from '../../validators/form-validator';
+import { AddProduct } from '../../common/add-product';
 
 @Component({
   selector: 'app-admin-add-product',
@@ -23,6 +25,76 @@ export class AdminAddProductComponent implements OnInit {
   
   
   ngOnInit(): void {
+    this.createProductFormGroup();
+  }
+
+  createProductFormGroup() {
+    this.productFormGroup = this.formBuilder.group({
+        name: ['', {validators: [Validators.required, Validators.minLength(4), FormValidator.checkNotOnlyWhitespace]}],
+        description: ['', {validators: [Validators.required, Validators.minLength(10), FormValidator.checkNotOnlyWhitespace]}],
+        categoryId: ['', {validators: [Validators.required]}],
+        unitPrice: [null, {validators: [Validators.required, Validators.min(0.5)]}],
+        imageUrl: ['', {validators: [Validators.required, FormValidator.checkNotOnlyWhitespace]}],
+        unitsInStock: [null, {validators: [Validators.required, Validators.min(1), Validators.pattern(/^\d+$/)]}],
+        isActive: [false]
+      });
+  }
+
+  createNewProduct() {
+    if (this.productFormGroup.invalid) {
+        this.productFormGroup.markAllAsTouched();
+        return;
+    }
     
+    this.isDisabled = true;
+    const name = this.productFormGroup.controls['name'].value;
+    const description = this.productFormGroup.controls['description'].value;
+    const unitPrice = this.productFormGroup.controls['unitPrice'].value;
+    const imageUrl = this.productFormGroup.controls['imageUrl'].value;
+    const unitsInStock = this.productFormGroup.controls['unitsInStock'].value;
+    const isActive = this.productFormGroup.controls['isActive'].value;
+    const newProduct = new AddProduct(name, description, unitPrice, imageUrl, unitsInStock, 1, isActive);
+        
+    this.productService.createProduct(newProduct).subscribe({
+        next: (data) => {
+          this.isDisabled = false;
+          this.productFormGroup.reset();
+        
+          this.router.navigateByUrl('/');
+          this.toastService.show({message: 'Product created successfully', className: 'bg-success-toast text-light' });
+        },
+        error: (err) => {
+          this.isDisabled = false;
+          this.toastService.show({message: `Error creating product: ${err.error.message}`, className: 'bg-danger text-light' });
+        }
+      });
+  }
+
+  get name() {
+    return this.productFormGroup.get('name');
+  }
+
+  get description() {
+    return this.productFormGroup.get('description');
+  }
+
+  get categoryId() {
+    return this.productFormGroup.get('categoryId');
+  }
+
+  get unitPrice() {
+    return this.productFormGroup.get('unitPrice');
+  }
+
+  get imageUrl() {
+    return this.productFormGroup.get('imageUrl');
+  }
+
+  get unitsInStock() {
+    return this.productFormGroup.get('unitsInStock');
+  }
+
+  get isActive() {
+    return this.productFormGroup.get('isActive');
   }
 }
